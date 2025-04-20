ce9d56f67f32ee63d38d556fb1d89c6a474e9f50
Filibiin10 <Filibiinfanax10@gmail.com>
2025-04-20 06:17:20 -0700

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,4 +1,5 @@
-import db from "../db.js";
+// import db from "../db.js";
+import initDB from '../db.js';
 import bcrypt from "bcryptjs";
 
 export const createUser = async ({
@@ -50,26 +51,26 @@ const values = [
 ];
 
 
-  await db.execute(query, values);
+  await initDB.execute(query, values);
 };
 
 
 // Find User by Email
 export const getUserByEmail = async (email) => {
-  const [users] = await db.execute("SELECT * FROM users WHERE email = ?", [email]);
+  const [users] = await initDB.execute("SELECT * FROM users WHERE email = ?", [email]);
   return users.length > 0 ? users[0] : null;
 };
 
 // Get User Profile
 export const getUserById = async (id) => {
-  const [users] = await db.execute("SELECT id, fullName, email, phone FROM users WHERE id = ?", [id]);
+  const [users] = await initDB.execute("SELECT id, fullName, email, phone FROM users WHERE id = ?", [id]);
   return users.length > 0 ? users[0] : null;
 };
 
 export const getUserByUserRef = async (userRef) => {
   try {
     console.log("Searching for usereff =", userRef); // log the final value
-    const [rows] = await db.query("SELECT * FROM users WHERE usereff = ?", [String(userRef)]);
+    const [rows] = await initDB.query("SELECT * FROM users WHERE usereff = ?", [String(userRef)]);
     console.log("Query Result:", rows);
     return rows.length > 0 ? rows[0] : null;
   } catch (err) {
