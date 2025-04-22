af81b9ddf8c573170deb0d474fda58be81952eec
filibiin10 <filibiinfanax10@gmail.com>
2025-04-22 01:24:55 -0700

    Resolved merge conflict in db.js

--- db.js
+++ db.js
@@ -1,22 +1,7 @@
 // db.js
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
-dotenv.config();
 
-<<<<<<< HEAD
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 5,
-//   queueLimit: 0,
-// });
-
-=======
->>>>>>> a4f753324f08edeb93e0a14e876ccb707b286e4f
+// Direct credentials without dotenv
 const pool = mysql.createPool({
   host: "mysql.gb.stackcp.com",
   port: 58164,
@@ -28,8 +13,6 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-<<<<<<< HEAD
-
 // Optional test connection
 (async () => {
   try {
@@ -41,17 +24,4 @@ const pool = mysql.createPool({
   }
 })();
 
-=======
-// Optional test connection
-(async () => {
-  try {
-    const connection = await pool.getConnection();
-    console.log("✅ MySQL Pool connected");
-    connection.release();
-  } catch (err) {
-    console.error("❌ MySQL connection error:", err.message);
-  }
-})();
-
->>>>>>> a4f753324f08edeb93e0a14e876ccb707b286e4f
 export default pool;
