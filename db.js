b88a5d4c0218bc41df19556c1818fe89c93e299b
Filibiin10 <Filibiinfanax10@gmail.com>
2025-04-22 05:15:32 -0700

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ import mysql from "mysql2/promise";
 
 // Direct credentials without dotenv
 const pool = mysql.createPool({
-  host: "mysql.gb.stackcp.com",
-  port: 58164,
-  user: "aleelo-353039380cf0",
+  host: "mysql.us.stackcp.com",
+  port: 54213,
+  user: "aleelo-353039388efb",
   password: "aleelo123",
-  database: "aleelo-353039380cf0",
+  database: "aleelo-353039388efb",
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0,
