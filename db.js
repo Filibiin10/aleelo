152acca91c7b1a98a904c9b5bee7728de9cfc41e
filibiin10 <filibiinfanax10@gmail.com>
2025-04-22 01:35:23 -0700

    first commit

--- db.js
+++ db.js
@@ -11,13 +11,10 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});git add db.js
-git commit -m "Add SSL config for MySQL on Render"
-git push
-
+  // ssl: {
+  //   rejectUnauthorized: false
+  // }
+});
 
 // Optional test connection
 (async () => {
